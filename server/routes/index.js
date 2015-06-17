63498d66235509b03159bb328ada2d573a439bbd
jxc14 <John.Chynoweth-contractor@nreca.coop>
2015-06-17 16:00:18 -0400

    add rambda latest reading

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,24 @@
+var R = require('ramda');
+
 module.exports = function(app) {
     var api = '/api';
+
+var maxReading = function (item, item2) {
+     if (item.speed > item2.speed)
+    {
+        return item
+    }
+    else
+    {
+      return item2
+    }
+  }
+
      var readings = [
-     {id: 1,city: "arlington", spped: "12", direction : "SW", reading : "5/12/2015"},
-     {id: 2,city: "arlington", spped: "14", direction : "NW", reading : "5/13/2015"},
-     {id: 3,city: "Arlington", spped: "12", direction : "NW", reading : "5/14/2015"},
-     {id: 4,city: "arlington", spped: "8", direction : "E", reading : "5/15/2015"},
+     {id: 1,city: "arlington", speed: 12, direction : "SW", reading : "5/12/2015"},
+     {id: 2,city: "arlington", speed: 14, direction : "NW", reading : "5/13/2015"},
+     {id: 3,city: "Arlington", speed: 1, direction : "NW", reading : "5/14/2015"},
+     {id: 4,city: "arlington", speed: 8, direction : "E", reading : "5/15/2015"},
       
 
      ];
@@ -12,6 +26,7 @@ module.exports = function(app) {
 
     app.get(api + '/reading/:id', getReading);
     app.get(api + '/readings', getReadings);
+    app.get(api + '/latestreading', getLatestReading);
 
     function getReading(req, res, next) {
         
@@ -25,4 +40,9 @@ module.exports = function(app) {
     function getReadings(req, res, next) {
        res.send(readings);
     }
+
+    function getLatestReading(req, res, next) {
+        res.send(R.reduce(maxReading, {speed: 0} , readings));
+    }
+
 };
