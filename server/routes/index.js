3f76e014714b5f343a3ff60d7b9ffe8f2bea841f
jxc14 <John.Chynoweth-contractor@nreca.coop>
2015-06-18 12:27:59 -0400

    formatting page

--- server/routes/index.js
+++ server/routes/index.js
@@ -19,9 +19,7 @@ var maxReading = function (item, item2) {
      {id: 1,city: "arlington", speed: 12, direction : "SW", reading : "5/12/2015"},
      {id: 2,city: "arlington", speed: 14, direction : "NW", reading : "6/13/2015"},
      {id: 3,city: "Arlington", speed: 1, direction : "NW", reading : "5/14/2015"},
-     {id: 4,city: "arlington", speed: 8, direction : "E", reading : "5/15/2015"},
-      
-
+     {id: 4,city: "arlington", speed: 8, direction : "E", reading : "5/15/2015"},  
      ];
 
 
