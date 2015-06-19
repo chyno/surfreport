47d2a3e0bc0a84575b1e1bb33cd72bcd13025a4c
jxc14 <John.Chynoweth-contractor@nreca.coop>
2015-06-19 13:19:52 -0400

    add login

--- server/app.js
+++ server/app.js
@@ -2,12 +2,52 @@
 var express = require('express');
 var app = express();
 
+var logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    session = require('express-session'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+   GoogleStrategy = require('passport-google');
+   
+
 exports.StartServer = function() {
   //console.log("hello from server");
 
 //app.use('/', express.static('/index.html'));
 app.use(express.static('./client'));
 app.use('/', express.static('./client/index.html'));
+
+//===============EXPRESS================
+// Configure Express
+app.use(logger('combined'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+app.use(session({secret: 'supernova', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Session-persisted message middleware
+app.use(function(req, res, next){
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
 routes = require('./routes/index')(app);
 var server = app.listen(3000, function () {
 
